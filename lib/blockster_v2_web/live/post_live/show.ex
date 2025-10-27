4985f1de14f8bb1953214065f5e32258e17f4813
Fly Dev <dev@fly.io>
2025-10-27 18:30:21 +0000

    create PostLive.Show for displaying individual posts

---
+++ lib/blockster_v2_web/live/post_live/show.ex
@@ -0,0 +1,30 @@
+defmodule BlocksterV2Web.PostLive.Show do
+  use BlocksterV2Web, :live_view
+
+  alias BlocksterV2.Blog
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _url, socket) do
+    post = Blog.get_post!(id)
+
+    # Increment view count
+    {:ok, updated_post} = Blog.increment_view_count(post)
+
+    {:noreply,
+     socket
+     |> assign(:page_title, post.title)
+     |> assign(:post, updated_post)}
+  end
+
+  @impl true
+  def handle_event("delete", _params, socket) do
+    {:ok, _} = Blog.delete_post(socket.assigns.post)
+
+    {:noreply, push_navigate(socket, to: ~p"/")}
+  end
+end
